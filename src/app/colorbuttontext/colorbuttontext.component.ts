bd0a710d57238e14b2a8b94c2a6a0ae96a8c55c3
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-05-01 01:12:30 +0200

    update

--- src/app/colorbuttontext/colorbuttontext.component.ts
+++ src/app/colorbuttontext/colorbuttontext.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-colorbuttontext',
   templateUrl: './colorbuttontext.component.html',
-  styleUrls: ['./colorbuttontext.component.css']
+  styleUrls: ['./colorbuttontext.component.css'],
 })
 export class ColorbuttontextComponent {
+  messages: string[] = ['Hello world!', 'Hello sturd!', 'Hello goord!'];
+  color: string = 'black';
 
+  currentMessage: string = this.messages[0];
+
+  changeMessage(index: number) {
+    if (index > 3) {
+    }
+    this.currentMessage == this.messages[index];
+  }
+
+  changeColor(newColor: string) {
+    this.color = newColor;
+  }
 }
