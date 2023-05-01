ad57e093e4bd075aea8a72098a1b35e729e732fc
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-05-02 00:50:31 +0200

    update

--- src/app/formstorage/formstorage.services.ts
+++ src/app/formstorage/formstorage.services.ts
@@ -7,25 +7,6 @@ export class SharedService {
   array: { name: string; surname: string; email: string; password: string }[] =
     [];
 
-  @ViewChild('nameInput') nameInput!: ElementRef;
-  @ViewChild('surnameInput') surnameInput!: ElementRef;
-  @ViewChild('emailInput') emailInput!: ElementRef;
-  @ViewChild('passwordInput') passwordInput!: ElementRef;
-
-  onSubmit() {
-    this.array.push({
-      name: this.nameInput.nativeElement.value,
-      surname: this.surnameInput.nativeElement.value,
-      email: this.emailInput.nativeElement.value,
-      password: this.passwordInput.nativeElement.value,
-    });
-    this.nameInput.nativeElement.value = '';
-    this.surnameInput.nativeElement.value = '';
-    this.emailInput.nativeElement.value = '';
-    this.passwordInput.nativeElement.value = '';
-    console.log(this.array);
-  }
-
   onRemove(index: number) {
     this.array.splice(index, 1);
   }
--- src/app/recievingcards/recievingcards.component.ts
+++ src/app/recievingcards/recievingcards.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { SharedService } from '../formstorage/formstorage.services';
 
 @Component({
   selector: 'app-recievingcards',
   templateUrl: './recievingcards.component.html',
   styleUrls: ['./recievingcards.component.css'],
 })
-export class RecievingcardsComponent {}
+export class RecievingcardsComponent {
+  constructor(public sharedService: SharedService) {}
+  array: { name: string; surname: string; email: string; password: string }[] =
+    [];
+  onRemove(index: number) {
+    this.array.splice(index, 1);
+  }
+}
