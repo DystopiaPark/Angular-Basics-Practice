366144df6402b3d1a785ef8f9f149c4b8982ed12
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-05-01 14:25:28 +0200

    update

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
+  name: string = '';
+  surname: string = '';
+  email: string = '';
+  password: string = '';
 
+  @ViewChild('nameInput') nameInput!: ElementRef;
+  @ViewChild('surnameInput') surnameInput!: ElementRef;
+  @ViewChild('emailInput') emailInput!: ElementRef;
+  @ViewChild('passwordInput') passwordInput!: ElementRef;
+
+  onSubmit() {
+    this.name = this.nameInput.nativeElement.value;
+    this.surname = this.surnameInput.nativeElement.value;
+    this.email = this.emailInput.nativeElement.value;
+    this.password = this.passwordInput.nativeElement.value;
+    this.nameInput.nativeElement.value = '';
+    this.surnameInput.nativeElement.value = '';
+    this.emailInput.nativeElement.value = '';
+    this.passwordInput.nativeElement.value = '';
+  }
 }
